e21e34fb71f5779ebb68b45fb2bcfa17d10894fd
XiaoYulong <xiaoyulong007@gmail.com>
2015-01-29 20:30:38 +0800

    add YLSweetAlertView

---
+++ YLSweetAlertView/YLSweetAlertView.h
@@ -0,0 +1,13 @@
+//
+//  YLSweetAlertView.h
+//  YLSweetAlertViewDemo
+//
+//  Created by 肖 玉龙 on 15/1/29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLSweetAlertView : UIView
+
+@end
---
+++ YLSweetAlertView/YLSweetAlertView.m
@@ -0,0 +1,21 @@
+//
+//  YLSweetAlertView.m
+//  YLSweetAlertViewDemo
+//
+//  Created by 肖 玉龙 on 15/1/29.
+//
+//
+
+#import "YLSweetAlertView.h"
+
+@implementation YLSweetAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
