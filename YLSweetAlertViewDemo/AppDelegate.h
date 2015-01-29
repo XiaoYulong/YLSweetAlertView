a12b4a1b2c8d7452d75afb098db628a6b7765a03
XiaoYulong <xiaoyulong007@gmail.com>
2015-01-29 19:43:03 +0800

    Initial Commit

---
+++ YLSweetAlertViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLSweetAlertViewDemo
+//
+//  Created by 肖 玉龙 on 15/1/29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLSweetAlertViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLSweetAlertViewDemo
+//
+//  Created by 肖 玉龙 on 15/1/29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
